7a5848390524f1e8012de0b051a24671f8987759
Lucas  Song <lucas7song@gmail.com>
2023-10-16 20:44:32 -0700

    add garden splash

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,116 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+// import useWindowDimensions from "../hooks/windowSize";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>L.M.F.G</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    // const { width } = useWindowDimensions();
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>L.M.F.G</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        Writings
-        From the Garden
-        <img src="">
+            <main>
+                <div className="bg-orange-600 flex flex-column w-100">
+                    {600 > 500 ? (
+                        <div className="bg-orange-900 w-100 h-auto">
+                            <div>
+                                {/* <div className="text-3xl">Welcome to L.M.F.G Webquarters.</div> */}
 
-        </img>
+                                <img
+                                    src="/desktop-garden.jpg"
+                                    width="full"
+                                    height="400"
+                                    object-fit="fill"
+                                />
+                            </div>
+                        </div>
+                    ) : (
+                        <div>
+                            <img
+                                src="/mobile-garden.jpg"
+                                width="200"
+                                height="200"
+                            />
+                        </div>
+                    )}
+                    <center>
+                        <div className="text-2xl"><b>
+                           L...M...F...G... </b></div>
+                        <ul>
+                            <li>
+                                <a href="https://blog.lmfg.org">blog</a>
+                            </li>
+                            <li>
+                                <a href="https://store.lmfg.org">
+                                    store (under construction)
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://lmfg.org">
+                                   ... 
+                                </a>
+                            </li>
+                        </ul>
+                    </center>
+                </div>
+            </main>
 
+            <style jsx>{`
+                main {
+                    padding: 5rem 0;
+                    flex: 1;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    background-color: #CC7722;
+                    width: full;
+                }
+                footer {
+                    width: 100%;
+                    height: 100px;
+                    border-top: 1px solid #eaeaea;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+                footer img {
+                    margin-left: 0.5rem;
+                }
+                footer a {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    text-decoration: none;
+                    color: inherit;
+                }
+                code {
+                    background: #fafafa;
+                    border-radius: 5px;
+                    padding: 0.75rem;
+                    font-size: 1.1rem;
+                    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                        DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New,
+                        monospace;
+                }
+            `}</style>
 
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Made with dirty hands in Fremont, CA
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+            <style jsx global>{`
+                html,
+                body {
+                    padding: 0;
+                    margin: 0;
+                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI,
+                        Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+                        Helvetica Neue, sans-serif;
+                }
+                * {
+                    box-sizing: border-box;
+                }
+            `}</style>
+        </div>
+    );
 }
