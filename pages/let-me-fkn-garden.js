50f15d576af84eb9de6dbac405b4ef75dd281d46
Lucas  Song <lucas7song@gmail.com>
2023-11-09 11:00:11 -0800

    goodbye profanity

--- pages/let-me-fkn-garden.js
+++ pages/let-me-fkn-garden.js
@@ -3,10 +3,11 @@ import styles from "../styles/Home.module.css";
 function LetMeFknGarden() {
     return (
         <div className={styles.container}>
-            <div className={styles.title}>Let Me Fucking Garden</div>
-            <div className={styles.description}>
-                please?
+            <div className={styles.title}>
+                Let Me <span className="bleep">Freaking</span>
+                Garden
             </div>
+            <div className={styles.description}>please?</div>
         </div>
     );
 }
